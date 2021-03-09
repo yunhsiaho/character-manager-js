401b9ecaf1c75803a0c562c048075c1a45b3a3f0
hsia <yunhsia.ho@gmail.com>
2021-03-09 09:31:18 +0100

    save

--- src/script.js
+++ src/script.js
@@ -158,3 +158,8 @@ document.querySelector("#file").addEventListener("change",(e) => {
     };
     reader.readAsDataURL(file)
 });
+
+var uploadAvatar = (event)=> {
+  var avatar = document.getElementById('output');
+  avatar.src = URL.createObjectURL(event.target.files[0]);
+};
